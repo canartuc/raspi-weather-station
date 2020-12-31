2ba37bdfc36be13b295364202ea41de56b3517f2
Can Artuc <canartuc@gmail.com>
2020-12-31 16:36:37 +0100

    Command line parameter reading added

--- airpollution.go
+++ airpollution.go
@@ -11,29 +11,29 @@ import (
 // AirPollution is the main struct of the data
 type AirPollution struct {
 	Coord struct {
-		Lon float32 `json:"lon"`
-		Lat float32 `json:"lat"`
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
 	} `json:"coord"`
 	List []struct {
 		Main struct {
 			Aqi int `json:"aqi"`
 		} `json:"main"`
 		Components struct {
-			Co   float32 `json:"co"`
-			No   float32 `json:"no"`
-			No2  float32 `json:"no2"`
-			O3   float32 `json:"o3"`
-			So2  float32 `json:"so2"`
-			Pm25 float32 `json:"pm2_5"`
-			Pm10 float32 `json:"pm10"`
-			Nh3  float32 `json:"nh3"`
+			Co   float64 `json:"co"`
+			No   float64 `json:"no"`
+			No2  float64 `json:"no2"`
+			O3   float64 `json:"o3"`
+			So2  float64 `json:"so2"`
+			Pm25 float64 `json:"pm2_5"`
+			Pm10 float64 `json:"pm10"`
+			Nh3  float64 `json:"nh3"`
 		} `json:"components"`
 		Dt int `json:"dt"`
 	} `json:"list"`
 }
 
 // GetAirPollution is getting air pollution data from OpenWeatherAPI
-func GetAirPollution(lat float32, lon float32, apiKey string) {
+func GetAirPollution(lat float64, lon float64, apiKey string) {
 	openWeatherURL := fmt.Sprintf("http://api.openweathermap.org/data/2.5/air_pollution?lat=%f&lon=%f&appid=%s",
 		lat, lon, apiKey)
 	res, err := http.Get(openWeatherURL)
--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"sync"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func(lat float32, lon float32, apiKey string) {
-		GetWeather(lat, lon, apiKey)
-		wg.Done()
-		GetAirPollution(lat, lon, apiKey)
-		wg.Done()
-	}(52.52, 13.41, "")
+	var lat, lon float64
+	var apikey string
+	flag.Float64Var(&lat, "lat", 52.52, "Latitude")
+	flag.Float64Var(&lon, "lon", 13.41, "Longitude")
+	flag.StringVar(&apikey, "apikey", "", "OpenWeather API Key")
+	flag.Parse()
 
-	wg.Add(1)
-	go func(lat float32, lon float32, apiKey string) {
-		GetAirPollution(lat, lon, apiKey)
-		wg.Done()
-	}(52.52, 13.41, "")
+	if len(apikey) > 0 {
+		var wg sync.WaitGroup
 
-	wg.Wait()
+		wg.Add(1)
+		go func(lat float64, lon float64, apiKey string) {
+			GetWeather(lat, lon, apiKey)
+			wg.Done()
+		}(lat, lon, apikey)
+
+		wg.Add(1)
+		go func(lat float64, lon float64, apiKey string) {
+			GetAirPollution(lat, lon, apiKey)
+			wg.Done()
+		}(lat, lon, apikey)
+
+		wg.Wait()
+	} else {
+		log.Fatal("You must provide apikey command line parameter to perform operation.")
+	}
 }
