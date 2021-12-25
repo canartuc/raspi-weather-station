0aba817de20a8852e8d5a78f24c77a4b6f7096de
Can Artuc <canartuc@gmail.com>
2021-12-25 18:39:44 +0100

    Templating

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"sync"
 )
 
+var templateName string
+
 func main() {
 	var lat, lon float64
 	var apikey string
+
 	flag.Float64Var(&lat, "lat", 52.52, "Latitude")
 	flag.Float64Var(&lon, "lon", 13.41, "Longitude")
 	flag.StringVar(&apikey, "apikey", "", "OpenWeather API Key")
+	flag.StringVar(&templateName, "template", "default", "Template name")
 	flag.Parse()
 
 	if len(apikey) > 0 {
@@ -36,6 +42,17 @@ func main() {
 	}
 
 	fs := http.FileServer(http.Dir("templates/"))
-	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	http.Handle("/asset/", http.StripPrefix("/asset/", fs))
+
+	http.HandleFunc("/", ServeTemplate)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ServeTemplate(w http.ResponseWriter, r *http.Request) {
+	tmplName := fmt.Sprintf("templates/%s.html", templateName)
+	tmpl := template.Must(template.ParseFiles(tmplName))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
